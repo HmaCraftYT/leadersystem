3eb0747657a9d656391003c772d9528ad8c200a8
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-12 22:53:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -360,7 +360,7 @@ var unmuteembeddm = new Discord.RichEmbed()
 }
 });
 client.on('message', message => {
-    if (message.content.startsWith("-invites")) {
+    if (message.content.startsWith("+invites")) {
     message.guild.fetchInvites()
     .then(invites => message.channel.send(`انت جبت   ${invites.find(invite => invite.inviter.id === message.author.id).uses} عضو لهاذا السيرفر`))
 
@@ -385,7 +385,7 @@ client.on('message', message => {
  [+help ! لمعرفة اوامر البوت ]
  [+invites ! لمعرفة عدد الاشخاص الذين دعوتهم للسيرفر ]
  [+invite ! للحصول على رابط لاضافة البوت ]
-
+ 
 
 {[اوامر الادارة]}
  
@@ -465,35 +465,6 @@ moment.locale('ar-TN');
 })
 }
      });
-client.on('message', message => {
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(0);
-    let prefix = '!!';
-    let xp = require("./xp.json");
-    
-if(cmd === `${prefix}level`) {
-if(!xp[message.author.id]){
-    xp[message.author.id] = {
-      xp: 0,
-      level: 1
-   };
- }
-   let curxp = xp[message.author.id].xp;
-   let curlvl = xp[message.author.id].level;
-   let nxtLvlXp = curlvl * 300;
-   let difference = nxtLvlXp - curxp;
- 
-   let lvlEmbed = new Discord.RichEmbed()
-   .setAuthor(message.author.username)
-   .setColor(purple)
-   .addField("Level", curlvl, true)
-   .addField("XP", curxp, true)
-   .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
- 
-   message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
-}
-});
 client.on('message', message =>{
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
