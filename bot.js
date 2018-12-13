8cea101d838cf2761d4cd045aa55eecf0a1226a4
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-13 13:56:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -533,4 +533,27 @@ client.on('message', function(msg) {
       msg.channel.send({embed:embed});
     }
   });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ var prefix = "+";
+ client.on('message',async Epic => {
+  if(Epic.content.startsWith(prefix + "svonline")) {
+  if(!Epic.guild.member(Epic.author).hasPermissions('MANAGE_CHANNELS')) return Epic.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!Epic.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return Epic.reply('❌ **ليس معي الصلاحيات الكافية**');
+  Epic.guild.createChannel(`Voice Online : [ ${Epic.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice Online Is Activation In ${Epic.guild.name}`);
+    c.overwritePermissions(Epic.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(` Online :  ${Epic.guild.members.filter(m => m.voiceChannel).size} .`)
+    },1000);
+  });
+  }
+});
 client.login(process.env.BOT_TOKEN);
